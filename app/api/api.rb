83d0a9fd6f2f09eec00e6a49ca9b73b4c96a4e27
aurelien herve <mail@aurelien-herve.com>
2014-04-17 11:03:22 +0200

    restricted access for apidoc

--- app/api/api.rb
+++ app/api/api.rb
@@ -2,14 +2,14 @@ require 'grape-swagger'
 
 class API < Grape::API
   format :json
+  version :v1, using: :header, vendor: :shapter
 
-  #Not sure wether this is very secure or not...
+  mount Shapter::Ping
+
   before do
-    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Origin'] = 'localhost'
     header['Access-Control-Request-Method'] = '*'
   end
-
-  version :v1, using: :header, vendor: :shapter
-  mount Shapter::Ping
   add_swagger_documentation
+
 end

72d8f499752c0e398e2c28119b1c5134def21fee
aurelien herve <mail@aurelien-herve.com>
2014-04-23 14:12:33 +0200

    clean

--- scripts/load_csv.rb
+++ scripts/load_csv.rb
@@ -1,5 +1,5 @@
-File.open("/home/aherve/Downloads/tags.csv").each_line do |line|
-  ll = line.chomp.split(",")
+File.open("/home/aherve/Downloads/items_and_tags.csv").each_line do |line|
+  ll = line.chomp.split(";")
   item = Item.new(name: ll.first)
   tags = ll.map do |t|
     Tag.find_or_create_by(name: t)

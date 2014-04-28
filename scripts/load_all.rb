bcdd553a9e9b07a70a6179a63ff5945be75390c7
aurelien herve <mail@aurelien-herve.com>
2014-04-28 12:13:55 +0200

    ignored imports

--- scripts/load_all.rb
+++ scripts/load_all.rb
@@ -10,7 +10,7 @@ Tag.delete_all
 #Load items and tags
 puts "reading items and tags"
 File.open("/home/aherve/Shapter/shapter-api/scripts/csvs/items_and_tags.csv").each_line do |line|
-  ll = line.chomp.split(";")
+  ll = line.chomp.split(";").map(&:strip)
 
   id = ll.first
   name = ll[1]
@@ -24,7 +24,7 @@ end
 
 puts "loading users"
 File.open("/home/aherve/Shapter/shapter-api/scripts/csvs/users.csv").each_line do |line|
-  ll = line.chomp.split(";")
+  ll = line.chomp.split(";").map(&:strip)
 
   @users[ll[2]] = User.new(
     firstname: ll[0],
@@ -40,7 +40,7 @@ end
 
 puts "loading comments"
 File.read('./scripts/csvs/comments.csv').split("##").each do |line|
-  ll = line.chomp.split("|")
+  ll = line.chomp.split("|").map(&:strip)
 
   @comments[ll.first] = Comment.new(
   content: ll[1].gsub('\"','"').gsub("\'","'"),
@@ -51,19 +51,19 @@ end
 
 puts "loading likes"
 File.open("/home/aherve/Shapter/shapter-api/scripts/csvs/likes.csv").each_line do |line|
-  ll = line.chomp.split(";")
+  ll = line.chomp.split(";").map(&:strip)
   @comments[ll.first].likers << ll[1..-1].map{|email| @users[email]} 
 end
 
 puts "loading dislikes"
 File.open("/home/aherve/Shapter/shapter-api/scripts/csvs/dislikes.csv").each_line do |line|
-  ll = line.chomp.split(";")
+  ll = line.chomp.split(";").map(&:strip)
   @comments[ll.first].dislikers << ll[1..-1].map{|email| @users[email]}
 end
 
 puts "loading quality"
 File.open('./scripts/csvs/qualite.csv').each_line do |line|
-  ll = line.chomp.split(";")
+  ll = line.chomp.split(";").map(&:strip)
 
   c = @comments[ll.first]
   c.quality_score = ll.last.to_i*20
@@ -71,7 +71,7 @@ end
 
 puts "loading work"
 File.open('./scripts/csvs/qualite.csv').each_line do |line|
-  ll = line.chomp.split(";")
+  ll = line.chomp.split(";").map(&:strip)
 
   c = @comments[ll.first]
   c.work_score = ll.last.to_i*20

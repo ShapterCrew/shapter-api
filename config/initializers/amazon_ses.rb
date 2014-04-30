1cdcad71dab00978fc22ab4c857942cb5fa46e39
aurelien herve <mail@aurelien-herve.com>
2014-04-30 17:02:54 +0200

    used proper servers

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../aws_credentials.rb', __FILE__)
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  region: "eu-west-1",
+  server: "email.eu-west-1.amazonaws.com",
   access_key_id: AWS.config.access_key_id,
   secret_access_key: AWS.config.secret_access_key

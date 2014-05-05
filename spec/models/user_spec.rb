cddf22fd81b72f2e856f1c297e88f2d3fe32b197
aurelien herve <mail@aurelien-herve.com>
2014-05-05 14:29:44 +0200

    users regex validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe User do
+  before(:each) do 
+    User.delete_all
+  end
+
+
+  it "should set and validates school" do 
+    u = FactoryGirl.build(:user)
+    u.schools = []
+
+    u.valid?.should be_false
+
+    u.email = 'valid@student.ecp.fr'
+    u.save
+    u.reload
+    puts u.errors
+    u.valid?.should be_true
+    u.schools.first.name.should == "Centrale Paris"
+
+  end
+
 end

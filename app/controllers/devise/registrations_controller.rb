f809ea10ae1616fbe9fa37d79f2624c56238a4e9
aurelien herve <mail@aurelien-herve.com>
2014-05-14 16:05:54 +0200

    removed /users/me redirection

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -18,13 +18,11 @@ class Devise::RegistrationsController < DeviseController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        #respond_with resource.sign_in_json, location: after_sign_up_path_for(resource)
-        redirect_to "/users/me"
+        respond_with resource.sign_in_json, location: after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         expire_data_after_sign_in!
-        #respond_with resource.sign_in_json, location: after_inactive_sign_up_path_for(resource)
-        redirect_to "/users/me"
+        respond_with resource.sign_in_json, location: after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords resource

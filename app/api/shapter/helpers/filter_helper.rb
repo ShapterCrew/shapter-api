54b697de13ad93e1c2620a655bdf57abd9ced4be
aurelien herve <mail@aurelien-herve.com>
2014-05-15 17:05:56 +0200

    description tag in reco

--- app/api/shapter/helpers/filter_helper.rb
+++ app/api/shapter/helpers/filter_helper.rb
@@ -52,7 +52,7 @@ module Shapter
           .reject{|tag,count| count < 2 }
           .reject{|tag,count| ary.include? tag.id.to_s}
         end.take(limit)
-        .map{|tag,count| {name: tag.name, id: tag.pretty_id, score: count, type: tag.type}}
+        .map{|tag,count| {name: tag.name, id: tag.pretty_id, score: count, type: tag.type, description: tag.description}}
       end
       #}}}
 

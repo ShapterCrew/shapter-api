c97ae97736a5239f4cf7003a3eca8354ceda02bb
aurelien herve <mail@aurelien-herve.com>
2014-05-19 10:27:54 +0200

    test

--- app/api/shapter/entities/item_short.rb
+++ app/api/shapter/entities/item_short.rb
@@ -1,14 +1,12 @@
 module Shapter
   module Entities
     class ItemShort < Grape::Entity
+      expose :diagrams_count
       expose :name
       expose :pretty_id, as: :id
       expose :comments_count    
       expose :subscribers_count 
-      #expose :avg_quality_score 
-      #expose :avg_work_score    
       expose :front_avg_diag, as: :averaged_diagram
-      expose :diagrams_count
     end
   end
 end

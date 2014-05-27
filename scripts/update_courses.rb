d9c2418e4cafc47b06ad14a4be71e9d8ffa537db
aurelien herve <mail@aurelien-herve.com>
2014-05-27 15:40:20 +0200

    update telecom cours

---
+++ scripts/update_courses.rb
@@ -0,0 +1,30 @@
+class ItemUpdater
+  attr_accessor :ll
+  def initialize(line)
+    @ll = line.chomp.split("\t").map(&:strip).compact
+  end
+
+  def name
+    [ll[0],ll[1]].join(" - ")
+  end
+
+  def tags
+    @tags ||= @ll.map{|name| Tag.find_or_create_by(name: name)}
+  end
+
+end
+
+File.read(ARGV[0]).each_line.map{|line| ItemUpdater.new(line)}.each do |item_up|
+
+  if item = Item.find_by(name: item_up.name)
+    item_up.tags.each{|t| item.tags << t}
+    puts r = if item.save and item_up.tags.map(&:save).reduce(:&)
+               "saved:\titem #{item_up.name}"
+             else
+               "ERROR:\t#{item.errors.messages}" + item_up.tags.map{|t| t.errors.messages}.compact
+             end
+  else
+    puts "ERROR:\titem #{item_up.name}\tNOT FOUND."
+  end
+
+end

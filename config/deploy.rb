b3ab84636e8ee4ee7b599816b42b548c5e8cbbf1
aurelien herve <mail@aurelien-herve.com>
2014-06-06 16:59:20 +0200

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app), in: :sequence, wait: 2 do
       # Your restart mechanism here, for example:
        execute :touch, release_path.join('tmp/restart.txt')
     end
@@ -57,13 +57,21 @@ namespace :deploy do
 
   after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "restart delayed_job daemon"
+  after :restart, :start_delayed_job_deamon do
+    on roles(delayed_job_roles) do
+      within release_path do 
+        with rails_env: fetch(:rails_env) do 
+          execute :bundle, :exec, :'bin/delayed_job', args, :restart
+        end
+      end
     end
+    #on roles(:web), in: :groups, limit: 3, wait: 10 do
+    #  # Here we can do anything such as:
+    #  # within release_path do
+    #  #   execute :rake, 'cache:clear'
+    #  # end
+    #end
   end
 
 end

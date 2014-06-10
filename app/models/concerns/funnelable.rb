93cde722d9870501117480e782cd05463a41b727
aurelien herve <mail@aurelien-herve.com>
2014-06-10 15:49:49 +0200

    constructor

---
+++ app/models/concerns/funnelable.rb
@@ -0,0 +1,34 @@
+module Funnelable
+  extend ActiveSupport::Concern
+
+  included do 
+
+    def validate_funnel_format(attr)
+
+      return unless funnel = self.send(attr)
+      
+      funnel.each.with_index do |h,i|
+        if h.is_a? Hash
+          errors.add(:field, "#{attr}[#{i}] should have name key") unless h.has_key?("name") or h.has_key?(:name)
+          errors.add(:field, "#{attr}[#{i}] should have tag_ids key") unless h.has_key?("tag_ids") or h.has_key?(:tag_ids)
+          if h.has_key?("default_types")
+            errors.add(:field, "#{attr}[#{i}] default_types should be an array") unless h["default_types"].is_a?(Array)
+          end
+        else
+          errors.add(:field, "#{attr}[#{i}] should be a hash")
+        end
+      end
+    end
+
+  end
+
+  module ClassMethods
+    def funnel_for(attr_name)
+      self.send(:field, attr_name, type: Array)
+      self.send(:validate) do |obj|
+        obj.validate_funnel_format(attr_name)
+      end
+    end
+  end
+
+end

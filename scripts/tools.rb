845b2b3ff0bb954d2180453d5d3db8c0f34de5d4
hulyce <ulysseklatzmann@gmail.com>
2014-06-16 16:50:49 +0200

    tools file addd

---
+++ scripts/tools.rb
@@ -0,0 +1,23 @@
+
+
+
+def double_first_name user
+  email = user.email
+  puts "email : #{email}"
+  full_name = email.split("@")[0]
+  puts "full name : #{full_name}"
+  first_name = full_name.split(".")[0].capitalize
+  last_name = full_name.split(".")[1].capitalize
+  puts "first name : #{first_name}"
+  puts "last name : #{last_name}"
+  puts "save ? (y)"
+  response = $stdin.gets.chomp.strip
+  if response == 'y'
+    user.firstname = first_name
+    user.lastname = last_name
+    puts "ok"
+    user.save
+    puts "lol"
+  end
+end
+

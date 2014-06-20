02bf436958f319907a8b7959c514866c3efac7d3
aurelien herve <mail@aurelien-herve.com>
2014-06-20 17:30:11 +0200

    removed stuff from other stuff

--- app/api/shapter/entities/diagram.rb
+++ app/api/shapter/entities/diagram.rb
@@ -3,8 +3,8 @@ module Shapter
     class Diagram < Grape::Entity
       expose :front_values
       expose :pretty_id, as: :id
-      expose :author, using: Shapter::Entities::UserShort
-      expose :item, using: Shapter::Entities::ItemId
+     # expose :author, using: Shapter::Entities::UserShort, if: {show_author: true}
+      #expose :item, using: Shapter::Entities::ItemId, unless: {show_item: true}
     end
   end
 end

ba83ee451015a8dc4ce1b2fc9d8a2741792eee28
aurelien herve <mail@aurelien-herve.com>
2014-06-24 16:03:24 +0200

    added merge item method

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,6 @@ class Item
   field :name, type: String
   field :description, type: String
   field :short_name, type: String
-  field :tags, type: Array
   field :requires_comment_score, type: Integer
 
   embeds_many :comments
@@ -22,8 +21,44 @@ class Item
     def touch
       Item.find_or_create_by(name: "__null__").touch
     end
+
+    # {{{ merge item t2 into item t1. Tag t2 will be destroyed
+    # item2 relations will be added to item1. 
+    # item1 attributes take priority over item2 attributes (name, description...etc)
+    def merge!(item1,item2)
+
+      new_attr = {
+        :name                 => item1.name.blank?        ? item2.name        : item1.name,
+        :description          => item1.description.blank? ? item2.description : item1.description,
+        :short_name           => item1.short_name.blank?  ? item2.short_name  : item1.short_name,
+        :tag_ids              => [item1,item2].map(&:tag_ids).reduce(:+).uniq,
+        :subscriber_ids       => [item1,item2].map(&:subscriber_ids).reduce(:+).uniq,
+        :interested_user_ids  => [item1,item2].map(&:interested_user_ids).reduce(:+).uniq,
+        :constructor_user_ids => [item1,item2].map(&:constructor_user_ids).reduce(:+).uniq,
+      }
+
+      if item1.update_attributes(new_attr)
+        item1.touch
+        [:comments, :diagrams, :shared_docs].each do |key|
+          item2.send(key).each do |stuff|
+            ss = stuff.dup
+            ss.item = item1
+            puts ss.errors.messages unless ss.save
+          end
+        end
+        item2.destroy
+        return true
+      else
+        puts item1.errors.messages
+        return false
+      end
+
+    end
+    #}}}
+
   end
 
+  before_destroy :custom_destroy_callbacks
   after_destroy :class_touch
   def class_touch
     Item.touch
@@ -99,9 +134,9 @@ class Item
 
   def avg_diag
     @avg_diag ||= (
-    d = raw_avg_diag.fill_with(50)
-    d.item = self
-    d
+      d = raw_avg_diag.fill_with(50)
+      d.item = self
+      d
     )
   end
 
@@ -137,6 +172,25 @@ class Item
   before_save :set_requires_comment_score
 
   private
+
+  def custom_destroy_callbacks
+
+    tags.each do |tag|
+      tag.update_attribute(:item_ids, tag.item_ids - [self.id])
+    end
+
+    subscribers.each do |sub|
+      sub.update_attribute(:item_ids, sub.item_ids - [self.id])
+    end
+
+    interested_users.each do |sub|
+      sub.update_attribute(:cart_item_ids, sub.cart_item_ids - [self.id])
+    end
+
+    constructor_users.each do |sub|
+      sub.update_attribute(:constructor_user_ids, sub.constructor_user_ids - [self.id])
+    end
+  end
 
   def set_requires_comment_score
     self.requires_comment_score = ( 10*interested_users_count - subscribers_count - 20*comments_count)

c228c1e81302cf4ed3527902cca2e928bc054e8e
Aurélien Hervé <mail@aurelien-herve.com>
2014-07-28 19:03:41 +0200

    restored schools index

---
+++ app/api/shapter/v7/schools.rb
@@ -0,0 +1,20 @@
+module Shapter
+  module V7
+    class Schools < Grape::API
+      format :json
+
+      namespace :schools do 
+
+        #{{{ index
+        desc "get a list of schools as an array of tags"
+        post do 
+          c = Category.find_or_create_by(code: :school)
+          present :schools, Tag.where(category: c), with: Shapter::Entities::Tag, entity_options: entity_options
+        end
+        #}}}
+
+      end
+
+    end
+  end
+end

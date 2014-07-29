f471074bc4ce647f5599fb278c64e557301f8efa
Aurélien Hervé <mail@aurelien-herve.com>
2014-07-29 19:41:22 +0200

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -91,7 +91,7 @@ class User
     if (who_asks.schools & self.schools).any?
       lastname
     else
-      schools.any? ? "from #{school.first.name}" : ""
+      schools.any? ? "from #{schools.first.name}" : ""
     end
   end
 

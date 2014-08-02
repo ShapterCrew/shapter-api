f2e69d979e1bef4b0b4eac0b5bfcdbca41103850
Aurélien Hervé <mail@aurelien-herve.com>
2014-08-02 12:36:14 +0200

    removed elasticsearch, added category_filter to tags/index

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,22 +2,21 @@ class Tag
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  include Mongoid::Elasticsearch
-
-  elasticsearch!({
-    index_mappings: {
-      name: {
-        type: 'multi_field',
-        fields: {
-          name:     {type: 'string', analyzer: 'snowball'},
-          raw:      {type: 'string', index: :not_analyzed},
-          suggest:  {type: 'completion'} 
-        }
-      },
-      tags: {type: 'string', include_in_all: false}
-    },
-    wrapper: :load
-  })
+  #include Mongoid::Elasticsearch
+  #elasticsearch!({
+  #  index_mappings: {
+  #    name: {
+  #      type: 'multi_field',
+  #      fields: {
+  #        name:     {type: 'string', analyzer: 'snowball'},
+  #        raw:      {type: 'string', index: :not_analyzed},
+  #        suggest:  {type: 'completion'} 
+  #      }
+  #    },
+  #    tags: {type: 'string', include_in_all: false}
+  #  },
+  #  wrapper: :load
+  #})
 
   include Funnelable
   funnel_for :signup_funnel

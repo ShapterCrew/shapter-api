41cbaaa792516a0bdfdb4929da1e461f14e6282f
Aurélien Hervé <mail@aurelien-herve.com>
2014-08-25 16:15:05 +0200

    added students_count, diagrams_count, and items_count to tags entity

--- app/api/shapter/entities/tag.rb
+++ app/api/shapter/entities/tag.rb
@@ -6,6 +6,10 @@ module Shapter
       expose :short_name, if: lambda{ |u,o| o[:entity_options]["tag"][:short_name]}
       expose :items     , using: Shapter::Entities::Item, if: lambda{ |u,o| o[:entity_options]["tag"][:items]}
       expose :category_code, as: :category  , if: lambda{ |u,o| o[:entity_options]["tag"][:category]} 
+
+      expose :students_count, if: lambda{ |u, o| o[:entity_options]["tag"][:students_count]}
+      expose :items_count   , if: lambda{ |u, o| o[:entity_options]["tag"][:items_count]}
+      expose :diagrams_count, if: lambda{ |u, o| o[:entity_options]["tag"][:diagrams_count]}
     end
   end
 end

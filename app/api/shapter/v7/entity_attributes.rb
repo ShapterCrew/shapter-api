cf7a8fdd4b95750b17fe3658e8c97e5ac14a603c
Aurélien Hervé <mail@aurelien-herve.com>
2014-08-28 16:09:27 +0200

    oups

--- app/api/shapter/v7/entity_attributes.rb
+++ app/api/shapter/v7/entity_attributes.rb
@@ -23,7 +23,7 @@ module Shapter
           present :course_builder, [:cart_items, :subscribed_items, :constructor_items].sort
           present :diagram, [:front_values, :author, :item].sort
           present :item, [:name, :description, :tags, :comments_count, :documents_count, :interested_users, :subscribers, :constructor_users, :current_user_subscribed, :current_user_has_in_cart, :current_user_has_in_constructor, :subscribers, :current_user_comments_count, :current_user_diagram, :this_user_has_diagram , :this_user_has_comment, :diagrams_count, :current_user_can_comment, :comments, :requires_comment_score, :shared_docs, :follower_friends, :averaged_diagram ].sort
-          present :shared_docs, [:name, :description, :file, :dlCount, :author].sort
+          present :shared_doc, [:name, :description, :file, :dlCount, :author].sort
           present :signup_permission, [:email, :school_names, :firstname, :lastname].sort
           present :tag, [:name, :short_name, :items, :category, :students_count, :items_count, :diagrams_count, :comments_count].sort
           present :user, [:image, :firstname, :lastname, :schools, :admin, :confirmed, :confirmed_student, :comments, :comments_likes_count, :comments_dislikes_count, :user_diagram, :sign_in_count, :provider, :is_fb_friend, :comments_count, :items_count, :diagrams_count].sort
